9026a00fe3341e3ab307cc8b37e1e4af5302909a
Exant64 <warpcauldronproject@gmail.com>
2026-02-24 21:32:30 +0100

    added missing extern "C" in accessory_get_id sample

--- accessory_get_id/main.cpp
+++ accessory_get_id/main.cpp
@@ -4,6 +4,6 @@ static int myAccessoryID;
 
 // the accessory ID fetching cannot go in EarlyLoad, since we need our code
 // to run once the accessory has already been registered through the JSONs
-__declspec(dllexport) void CWEAPI_Load(CWE_API* pAPI) {
+extern "C" __declspec(dllexport) void CWEAPI_Load(CWE_API* pAPI) {
     myAccessoryID = pAPI->pRegister->pAccessory->GetAccessoryIndex("your_accessory_id_here");
 }
